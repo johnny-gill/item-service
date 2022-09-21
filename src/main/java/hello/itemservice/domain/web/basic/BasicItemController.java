5bc22b006cace59a2e93b77dd91b441117d01f04
gill <uint777@gmail.com>
2022-09-22 01:12:00 +0900

    PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -60,10 +60,17 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+//        return " basic/item";
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
